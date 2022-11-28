dbcb725920fd29b81f3d499661cf0780dab47e10
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-11-28 14:29:12 +0000

    28/11/2022

--- TelephoneList/class.cpp
+++ TelephoneList/class.cpp
@@ -12,3 +12,104 @@ bool TelList::append(const string &name,const string &number)
         
         return false;
     }
+
+bool TelList::erase(const string &key)
+    {
+        int i=search(key);
+        if(i!=pseudo)
+        {
+            v[i]=v[count-1]; --count;
+            return true;
+        }
+        
+        return false;
+    }
+
+int TelList::search(const string &key)
+    {
+        for(int i=0;i<count;i++)
+            if(v[i].name==key)
+                return i;
+            
+        return pseudo;
+    }
+
+inline void tab_header()
+    {
+        cout<<"\nName\t\t\tTelephone#\n"
+            "--------------------------------\n";
+    }
+
+inline void print_line(const Element &el)
+    {
+        cout<<left<<setw(30)<<el.name.c_str()
+            <<left<<setw(20)<<el.number.c_str()<<endl;
+    }
+
+void TelList::print()
+    {
+        if(count==0)
+            cout<<"\nEmpty\n";
+        else
+        {
+            tab_header();
+            for(int i=0;i<count;++i)
+                print_line(v[i]);
+        }
+    }
+
+int TelList::print(const string &name)
+    {
+        int matches=0,len=name.length();
+
+        for(int i=0;i<count;++i)
+        {
+            if(v[i].name.compare(0,len,name)==0)
+            {
+                if(matches==0)tab_header();
+                ++matches;
+                print_line(v[i]);
+            }
+        }
+        if(matches==0)
+            cout<<"No matches\n";
+        
+        return matches;
+    }
+
+int TelList::get_new()
+    {
+        int input=0;
+
+        cout<<"\nenter new name and number\n";
+
+        Element el;
+        while(true)
+        {
+            cout<<"new lastname, firstname:";
+            cin.sync();getline(cin,el.name);
+
+            if(el.name.empty())
+                break;
+            
+            cout<<"\nnumber";
+            cin.sync();getline(cin,el.number);
+            if(!append(el))
+            {
+                cout<<"\nname not fount\n";
+                if(count==max)
+                {
+                    cout<<"table is full\n";
+                    break;
+                }
+                if(search(el.name)!=pseudo)
+                    cout<<"name exists\n";
+            }
+            else
+            {
+                ++input;
+                cout<<"new element inserted\n";
+            }
+        }
+        return input;
+    }
--- TelephoneList/main.cpp
+++ TelephoneList/main.cpp
@@ -1,1 +1,95 @@
 #include "class.cpp"
+#include <cctype>
+// controler functions
+
+inline void clear()
+{
+    cout<<"\033[2j\n";
+}
+
+inline void go_on()
+{
+    cout<<"\ngo on with return";
+    cin.sync(); cin.clear();
+    while(cin.get()!='\n');
+}
+
+char header[]="\n\n\t\t***telephone list****\n\n";
+TelList friends;
+
+int menu()
+{
+    static char menu[]=
+        "\n\n\t\tD=Display all entries"
+        "\n\n\t\tF=find a number"
+        "\n\n\t\tA=append a new entrie"
+        "\n\n\t\tE=erase an entrie"
+        "\n\n\t\tQ=quit program"
+        "\n\nyour choice";
+
+        clear();
+        cout<<header;
+
+        char choice;
+        cin.sync();cin.clear();
+        if(!cin.get(choice))
+            choice='B';
+        else 
+            choice=toupper(choice);
+
+        cin.sync();
+        return choice;
+}
+
+int main()
+{
+    int action=0;
+    string name;
+
+    friends.append("lucky luuukk", "599288177");
+
+    while(action !='B')
+    {
+        action=menu();
+        clear();
+
+        cout<<header<<endl;
+
+        switch(action)
+        {
+            case 'D':
+                friends.print();
+                go_on();
+            break;
+            case 'F':
+                cout<<"enter name";
+                getline(cin,name);
+                if(!name.empty())
+                {
+                    friends.print(name);
+                    go_on();
+                }
+            break;
+            case 'A':
+                friends.get_new();
+            break;
+            case 'E':
+                cout<<"enter name";
+                getline(cin,name);
+                if(!name.empty())
+                {
+                    if(!friends.erase(name))
+                        cout<<name<<"no found\n";
+                    else    
+                        cout<<"entry for "<<name<<" deleted\n";
+                    
+                    go_on();
+                }
+            break;
+            case 'T': 
+                clear();
+                break;
+        }
+    }
+    return 0;
+}
