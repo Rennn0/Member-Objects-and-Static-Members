01dd6a7800146b926de3107e744da8a40a5715a2
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-11 10:02:53 +0000

    .

---
+++ overload/fraction.h
@@ -0,0 +1,15 @@
+#include "../members/library.h"
+
+/*
+    class with numerator and a denominator of type long
+    the constructor has two parameters of type  long: the first
+    parameter (numerator) contains the default value 0, and second
+    parameter(denominator) contains the value 1. declare operator
+    functions as methods for unary ++ --(both pre) += -= *= /=
+    binary + - * /  and as frineds >> <<
+*/
+
+class Fraction
+{
+
+};
