1b46926e96279c2e54a422838b088ce599a50be3
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-11 11:27:04 +0000

    class with overloaded arithmetics

--- overload/fraction.cpp
+++ overload/fraction.cpp
@@ -1,1 +1,63 @@
 #include "fraction.h"
+
+    Fraction operator+(const Fraction& a, const Fraction& b)
+    {
+        Fraction temp;
+        temp.denominator=a.denominator*b.denominator;
+        temp.numerator=a.numerator*b.denominator+b.numerator*a.denominator;
+        cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        return temp;
+    }
+    Fraction operator-(const Fraction& a, const Fraction& b)
+    {
+        Fraction temp;
+        temp.denominator=a.denominator*b.denominator;
+        temp.numerator=a.numerator*b.denominator-b.numerator*a.denominator;
+        cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        return temp;
+    }
+    Fraction operator*(const Fraction& a, const Fraction& b)
+    {
+        Fraction temp;
+        temp.denominator=a.denominator*b.denominator;
+        temp.numerator=a.numerator*b.numerator;
+        cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        return temp;
+    }
+    Fraction operator/(const Fraction& a, const Fraction& b)
+    {
+        Fraction temp;
+        temp.denominator=a.denominator*b.numerator;
+        temp.numerator=a.numerator*b.denominator;
+        cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        return temp;
+    }
+    istream& operator>>(istream& is, Fraction& obj)
+    {
+        cout<<"Numerator_ ";
+        while(!(is>>obj.numerator))
+        {
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"try again_ ";
+        }
+        cout<<"Denominator_";
+        while(!(is>>obj.denominator) || obj.denominator==0)
+        {
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"try again_ ";
+        }
+        return is;
+    }
+    ostream& operator<<(ostream& os, const Fraction& obj)
+    {
+        cout<<obj.numerator<<' '<<obj.denominator<<endl;
+    }
+    
+int main()
+{
+    Fraction obj(2,3), obj2(4,5),obj3;
+
+    obj/=obj2;
+}
--- overload/fraction.h
+++ overload/fraction.h
@@ -11,5 +11,65 @@
 
 class Fraction
 {
+    private:
+        long numerator, denominator;
+    public:
+        Fraction(long n=0, long d=1)
+        {
+            if(d==0)
+            {
+                cerr<<"u dumb?"<<endl; exit(1);
+            }
+            numerator=n, denominator=d;
+        }
+        Fraction& operator ++()
+        {
+            numerator+=denominator;
+            cout<<numerator<<' '<<denominator<<endl; 
+            return *this;
+        }
+        Fraction& operator --()
+        {
+            numerator-=denominator; 
+            cout<<numerator<<' '<<denominator<<endl;
+            return *this;
+        }
+        // lets forget about math from here
+        Fraction& operator +=(const Fraction& obj)
+        {   
+            this->numerator=
+            this->numerator*obj.denominator+obj.numerator*this->denominator;
+            this->denominator*=obj.denominator;
+            cout<<numerator<<' '<<denominator<<endl;
+            return *this;
+        }
+        Fraction& operator -=(const Fraction& obj)
+        {
+            this->numerator=
+            this->numerator*obj.denominator-obj.numerator*this->denominator;
+            this->denominator*=obj.denominator;
+            cout<<numerator<<' '<<denominator<<endl;
+            return *this;
+        }
+        Fraction& operator *=(const Fraction& obj)
+        {
+            this->numerator*=obj.numerator;
+            this->denominator*=obj.denominator;
+            cout<<numerator<<' '<<denominator<<endl;
+            return *this;
+        }
+        Fraction& operator /=(const Fraction& obj)
+        {
+            this->numerator*=obj.denominator*1.;
+            this->denominator*=obj.numerator*1.;
+            cout<<numerator<<' '<<denominator<<endl;
+            return *this;
+        }  
+        friend Fraction operator+(const Fraction&,const Fraction&);
+        friend Fraction operator-(const Fraction&,const Fraction&);
+        friend Fraction operator/(const Fraction&,const Fraction&);
+        friend Fraction operator*(const Fraction&,const Fraction&);
+        friend ostream& operator<<(ostream&,const Fraction&);
+        friend istream& operator>>(istream&,Fraction&);
 
 };
