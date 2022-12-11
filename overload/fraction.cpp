5bf09aca1acedf5c6ce2679ba69e74741d5538ae
luka danelia <104508895+Rennn0@users.noreply.github.com>
2022-12-11 11:45:59 +0000

    friend functions overlaoded

--- overload/fraction.cpp
+++ overload/fraction.cpp
@@ -6,6 +6,7 @@
         temp.denominator=a.denominator*b.denominator;
         temp.numerator=a.numerator*b.denominator+b.numerator*a.denominator;
         cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        temp.value();
         return temp;
     }
     Fraction operator-(const Fraction& a, const Fraction& b)
@@ -14,6 +15,7 @@
         temp.denominator=a.denominator*b.denominator;
         temp.numerator=a.numerator*b.denominator-b.numerator*a.denominator;
         cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        temp.value();
         return temp;
     }
     Fraction operator*(const Fraction& a, const Fraction& b)
@@ -22,6 +24,7 @@
         temp.denominator=a.denominator*b.denominator;
         temp.numerator=a.numerator*b.numerator;
         cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        temp.value();
         return temp;
     }
     Fraction operator/(const Fraction& a, const Fraction& b)
@@ -30,6 +33,7 @@
         temp.denominator=a.denominator*b.numerator;
         temp.numerator=a.numerator*b.denominator;
         cout<<temp.numerator<<' '<<temp.denominator<<endl;
+        temp.value();
         return temp;
     }
     istream& operator>>(istream& is, Fraction& obj)
@@ -48,16 +52,22 @@
             is.ignore(numeric_limits<streamsize>::max(),'\n');
             cout<<"try again_ ";
         }
+        obj.value();
         return is;
     }
     ostream& operator<<(ostream& os, const Fraction& obj)
     {
         cout<<obj.numerator<<' '<<obj.denominator<<endl;
+        obj.value();
+        return os;
     }
     
 int main()
 {
     Fraction obj(2,3), obj2(4,5),obj3;
+    // cout<<obj<<obj2<<obj3;
 
-    obj/=obj2;
+    // ++obj, --obj2, obj3=obj+obj2;
+    
+    // cin>>obj3;
 }
